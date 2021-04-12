b31da96a5572061f85f0ba697e13f550d8b3d164
fedia-hamdi <hamdifediasimplonenda@gmail.com>
2021-04-12 15:27:01 +0100

    add navbar + section 1 + section 2 section 3

--- js/script.js
+++ js/script.js
@@ -2,11 +2,15 @@ window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
+var logo = document.getElementsByClassName("logo");
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
+    logo[0].classList.add("log")
+
   } else {
     navbar.classList.remove("sticky");
+    logo[0].classList.remove("log")
   }
 }
